8dd7f568712213f4b381878afcf5854437802b56
sylvaincodes <sylvaincodeur@gmail.com>
2023-09-08 10:10:33 +0100

    maj api

--- index.js
+++ index.js
@@ -32,11 +32,37 @@ const app = express();
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors(
-  {
-     origin: "*",
-     methods : ['POST', 'UPDATE'] 
-    }));
+// app.use(cors(
+//   {
+//      origin: "*",
+//      methods : ['POST', 'UPDATE']
+//     }));
+
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.post("/create-intent", async (req, res) => {
   const amount = req.body.amount;
